ab931c09019e1019156fca7b89c4e53818b566ff
Rayed Benbrahim <rayed.benbrahim@gmail.com>
2018-05-17 17:15:17 +0200

    shopping and recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('First Recipe', 'Test recipe for our app', 'https://c1.staticflickr.com/6/5186/5803041832_40261728ce_b.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
