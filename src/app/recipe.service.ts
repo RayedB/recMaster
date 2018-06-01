0ea79c783c9c272d69d5e6066f9349e104bd3172
Rayed Benbrahim <rayed.benbrahim@gmail.com>
2018-06-01 16:21:53 +0200

    add recipeService

---
+++ src/app/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipes/recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('First Recipe', 'Test recipe for our app', 'https://c1.staticflickr.com/6/5186/5803041832_40261728ce_b.jpg'),
+    new Recipe('Second Recipe', 'Test recipe for our app', 'http://www.dreams.metroeve.com/wp-content/uploads/2017/08/www.dreams.metroeve.com-lasagna-dreams-meaning.jpg'),
+    new Recipe('Third Recipe', 'Test recipe for our app', 'https://img.buzzfeed.com/buzzfeed-static/static/2016-06/8/9/campaign_images/buzzfeed-prod-web09/23-boneless-chicken-breast-recipes-that-are-actua-2-30592-1465393955-1_dblbig.jpg'),
+  ];
+  constructor() { }
+}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { RecipeService } from '../../recipe.service';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,19 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeHasBeenSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
-    new Recipe('First Recipe', 'Test recipe for our app', 'https://c1.staticflickr.com/6/5186/5803041832_40261728ce_b.jpg'),
-    new Recipe('Second Recipe', 'Test recipe for our app', 'http://www.dreams.metroeve.com/wp-content/uploads/2017/08/www.dreams.metroeve.com-lasagna-dreams-meaning.jpg'),
-    new Recipe('Third Recipe', 'Test recipe for our app', 'https://img.buzzfeed.com/buzzfeed-static/static/2016-06/8/9/campaign_images/buzzfeed-prod-web09/23-boneless-chicken-breast-recipes-that-are-actua-2-30592-1465393955-1_dblbig.jpg'),
-  ];
-  constructor() { }
+  recipes: Recipe[] = [];
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onSelectedRecipe(recipe: Recipe){
-    this.recipeHasBeenSelected.emit(recipe)
+    this.recipes = this.recipeService.recipes;
   }
 
 }
